224b39eca3dbc8f1bff5b730d7d352df9200b401
ronbarrantes <ronbarrantes@gmail.com>
2024-03-30 10:24:59 -0700

    Add new files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ['selector', '[dark-mode="dark"]'],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -18,3 +19,4 @@ const config: Config = {
   plugins: [],
 };
 export default config;
+
